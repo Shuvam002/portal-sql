64d3da52af997bcf960abc68af6ec03bd7aa9384
Shuvam Sen <72847016+Shuvam002@users.noreply.github.com>
2023-03-29 22:15:40 +0530

    Delete crd.js

--- routes/crd.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const crdcontrollers = require('../controllers/crd');
-
-router.post('/', crdcontrollers.getData);
-router.put('/:id', crdcontrollers.updateData);
-
-module.exports = router;
