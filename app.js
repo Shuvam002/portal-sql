cfd275b241d9c46fe5086919cab475eb6f52c0ff
Shuvam Sen <72847016+Shuvam002@users.noreply.github.com>
2023-03-29 22:05:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,124 @@
 const express = require('express');
 const cors = require('cors');
 const bodyp = require('body-parser');
-
-
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'I live@malda7321011911',
+    database: 'test',
+  });
 
 const app = express();
 app.use(express.json());
 app.use(express.static('public'))
 app.use(cors());
-app.use(bodyp.urlencoded({extended:true}));
+app.use(bodyp.urlencoded({ extended: true }));
 const mrd_routes = require('./routes/mrd');
 const rd_routes = require('./routes/rd');
-const crd_routes = require('./routes/crd');
+
 // const crd_routes = require('./routes/crd');
 // const fetch_routes = require('./routes/fetch');
 
 
 
 
-app.use('/mrd.html',mrd_routes);
+app.use('/mrd.html', mrd_routes);
 app.use('/rd.html', rd_routes);
-app.use('/crd.html',crd_routes);
+app.get('/crd.html', (req, res) => {
+    connection.query('SELECT DISTINCT event FROM rd', (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error retrieving events from database');
+        } else {
+            const events = results.map((result) => result.event);
+            res.send(`
+          <html>
+            <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>techxtra registration</title>
+              <style>
+                table, th, td {
+                  border: 1px solid;
+                }
+              </style>
+            </head>
+            <body>
+              <form action="/crd.html" method="post">
+                <select name="event">
+                  ${events.map((event) => `<option value="${event}">${event}</option>`)}
+                </select>
+                <button type="submit">Submit</button>
+              </form>
+            </body>
+          </html>
+        `);
+        }
+    });
+});
+
+app.post('/crd.html', (req, res) => {
+    const event = req.body.event;
+    connection.query(`SELECT * FROM rd WHERE event='${event}' and played = 0`, (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error retrieving data from database');
+        } else {
+            const rows = results.map((result) => `
+          <tr>
+            <td>${result.id}</td>
+            <td><input type="text"></td>
+            <td><button onclick="markID(${result.id})">Mark</button></td>
+          </tr>
+        `).join('');
+            res.send(`
+          <html>
+            <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>techxtra registration</title>
+              <style>
+                table, th, td {
+                  border: 1px solid;
+                }
+              </style>
+              <script>
+                function markID(id) {
+                  fetch('/mark', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ id: id }),
+                  }).then(() => location.reload());
+                }
+              </script>
+            </head>
+            <body>
+              <table>
+                ${rows}
+              </table>
+            </body>
+          </html>
+        `);
+        }
+    });
+});
+
+app.post('/mark', (req, res) => {
+    console.log('mark');
+    const id = req.body.id;
+    console.log(id)
+    const update = 'UPDATE rd SET played=1 WHERE id=?';
+    console.log(connection.query(update, [id]));
+    connection.query(update, id, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error updating data in database');
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
 // app.use('/fetch',fetch_routes);
 // app.use('/crd',crd_routes);
 app.listen(3000);
--- controllers/rd.js
+++ controllers/rd.js
@@ -12,38 +12,73 @@ const addrd = async (req, res) => {
 
     try {
         const connection = await getConnection();
+        if (event === 'combos') {
+            const sqlInsert = 'INSERT INTO rd (Domain,event,gid1,gid2,gid3,gid4,gid5,phone) VALUES (?, ?, ?, ?, ?, ?, ?)';
+            const sqlSelect = 'SELECT gid1, gid2, gid3, gid4, gid5 FROM rd WHERE event = ?';
+            const sqlParams = [gid1, gid2, gid3, gid4, gid5, phone, event];
+            connection.query(sqlSelect, [event], (err, result) => {
+                if (err) throw err;
 
-        
-        let checkDuplicationSql = 'SELECT gid1, gid2, gid3, gid4, gid5 FROM rd WHERE event = ? AND (';
-        let params = [event];
-        let fees = [];
-        const gids = [gid1, gid2, gid3, gid4, gid5].filter(gid => gid);
-        checkDuplicationSql += gids.map(gid => `gid1 IN (?)`).join(' OR ') + ' OR ';
-        checkDuplicationSql += gids.map(gid => `gid2 IN (?)`).join(' OR ') + ' OR ';
-        checkDuplicationSql += gids.map(gid => `gid3 IN (?)`).join(' OR ') + ' OR ';
-        checkDuplicationSql += gids.map(gid => `gid4 IN (?)`).join(' OR ') + ' OR ';
-        checkDuplicationSql += gids.map(gid => `gid5 IN (?)`).join(' OR ');
-        params.push(...gids, ...gids, ...gids, ...gids, ...gids);
+                const arr = [];
+                result.forEach(row => {
+                    if (row.gid1) arr.push(row.gid1);
+                    if (row.gid2) arr.push(row.gid2);
+                    if (row.gid3) arr.push(row.gid3);
+                    if (row.gid4) arr.push(row.gid4);
+                    if (row.gid5) arr.push(row.gid5);
+                });
 
-        checkDuplicationSql += ') AND event = ?';
-        params.push(event);
-        
-        const [duplicateRows] = await connection.execute(checkDuplicationSql, params);
-        console.log(duplicateRows);
-        if (duplicateRows.length > 0) {
-            res.status(400).json({ error: 'Record already exists' ,duplicateRows});
-            return;
+                connection.query(sqlInsert, sqlParams, (err, result) => {
+                    connection.release();
+
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('Error inserting record');
+                        return;
+                    }
+
+                    const lastId = result.insertId;
+                    const responseText = `Your TID for ${event} is ${lastId}`;
+
+                    if (arr.includes(gid1) || arr.includes(gid2) || arr.includes(gid3) || arr.includes(gid4) || arr.includes(gid5)) {
+                        res.send(`${responseText}<br>GID already exists in this event`);
+                    } else {
+                        res.send(responseText);
+                    }
+                });
+            });
+        } else {
+            let checkDuplicationSql = 'SELECT gid1, gid2, gid3, gid4, gid5 FROM rd WHERE event = ? AND (';
+            let params = [event];
+            let fees = [];
+            const gids = [gid1, gid2, gid3, gid4, gid5].filter(gid => gid);
+            checkDuplicationSql += gids.map(gid => `gid1 IN (?)`).join(' OR ') + ' OR ';
+            checkDuplicationSql += gids.map(gid => `gid2 IN (?)`).join(' OR ') + ' OR ';
+            checkDuplicationSql += gids.map(gid => `gid3 IN (?)`).join(' OR ') + ' OR ';
+            checkDuplicationSql += gids.map(gid => `gid4 IN (?)`).join(' OR ') + ' OR ';
+            checkDuplicationSql += gids.map(gid => `gid5 IN (?)`).join(' OR ');
+            params.push(...gids, ...gids, ...gids, ...gids, ...gids);
+
+            checkDuplicationSql += ') AND event = ?';
+            params.push(event);
+
+            const [duplicateRows] = await connection.execute(checkDuplicationSql, params);
+            console.log(duplicateRows);
+            if (duplicateRows.length > 0) {
+                res.status(400).json({ error: 'Record already exists', duplicateRows });
+                return;
+            }
+
+
+            const sql = 'INSERT INTO rd (Domain, event, gid1, gid2, gid3, gid4, gid5, phone) VALUES (?,?,?,?,?,?,?,?)';
+            const values = [Domain, event, gid1, gid2, gid3, gid4, gid5, phone];
+
+            const [result] = await connection.execute(sql, values.map((val) => val === undefined ? null : val));
+            var msg = fees.map((fee) => fee === event);
+            const userId = result.insertId;
+
+            res.json("Your TID is:" + userId + " " + msg);
         }
-
-
-        const sql = 'INSERT INTO rd (Domain, event, gid1, gid2, gid3, gid4, gid5, phone) VALUES (?,?,?,?,?,?,?,?)';
-        const values = [Domain, event, gid1, gid2, gid3, gid4, gid5, phone];
-
-        const [result] = await connection.execute(sql, values.map((val) => val === undefined ? null : val));
-        var msg=fees.map((fee)=>fee===event);
-        const userId = result.insertId;
-
-        res.json("Your TID is:" + userId+" "+msg);
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Internal server error' });
