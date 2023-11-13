3affa5f1acdf4a83b52fd6cb59d68331052cf54f
Shuvam Sen <72847016+Shuvam002@users.noreply.github.com>
2023-11-13 09:53:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyp = require('body-parser');
 const mysql = require('mysql2');
-const connection = require('./db/conn.js');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
 
 const app = express();
 app.use(express.json());
